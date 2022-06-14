6ac306ffaf2a2757d9cbe4f543fc8984926af564
Kento Nakayama <kento427@outlook.com>
2022-06-14 21:08:30 +0900

    remove # from Unistalles-es

--- AVD/Uninstalles-es.ps1
+++ AVD/Uninstalles-es.ps1
@@ -1,7 +1,7 @@
 ##Cleanup to prepare sysprep##
-#Remove-AppxPackage -Package Microsoft.LanguageExperiencePackes-ES_22000.8.13.0_neutral__8wekyb3d8bbwe
+Remove-AppxPackage -Package Microsoft.LanguageExperiencePackes-ES_22000.8.13.0_neutral__8wekyb3d8bbwe
 
-#Remove-AppxPackage -Package Microsoft.OneDriveSync_22000.8.13.0_neutral__8wekyb3d8bbwe
+Remove-AppxPackage -Package Microsoft.OneDriveSync_22000.8.13.0_neutral__8wekyb3d8bbwe
 
 $LangList = Get-WinUserLanguageList
 $MarkedLang = $LangList | where LanguageTag -eq "en-US"
